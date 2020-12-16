dc49eb87ce45776ad2b87af7268b5c0cad81bcca
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2020-12-16 15:43:13 +0100

    Initial project setup

---
+++ src/main/java/com/almgru/expm/Main.java
@@ -0,0 +1,5 @@
+package com.almgru.expm;
+
+public class Main {
+    public static void main(String[] args) { }
+}
---
+++ src/test/java/com/almgru/expm/MainTest.java
@@ -0,0 +1,12 @@
+package com.almgru.expm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+    @Test
+    public void junitIsSetupCorrectly() {
+        assertTrue(true);
+    }
+}
