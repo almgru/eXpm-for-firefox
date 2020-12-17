2d3d1e33a80019f77eda7ea9f5a9eaa9b707f0d9
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2020-12-17 09:25:52 +0100

    Adds main window

--- src/main/java/com/almgru/expm/Main.java
+++ src/main/java/com/almgru/expm/Main.java
@@ -1,5 +1,28 @@
 package com.almgru.expm;
 
-public class Main {
-    public static void main(String[] args) { }
+import com.almgru.expm.view.MainWindow;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        MainWindow mainWindow = null;
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainWindow.fxml"));
+            primaryStage.setScene(new Scene(loader.load()));
+            mainWindow = loader.getController();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            System.exit(1);
+        }
+
+        primaryStage.show();
+    }
 }
