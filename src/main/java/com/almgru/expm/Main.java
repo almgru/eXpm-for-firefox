532216be05dad61eaece8d8a2d539a545c5db765
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-11 19:51:46 +0100

    Refactors Main

--- src/main/java/com/almgru/expm/Main.java
+++ src/main/java/com/almgru/expm/Main.java
@@ -20,41 +20,63 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        MainWindow mainWindow;
-        Collection<Profile> profiles = null;
         PathUtils pathUtils = new PathUtils();
 
         try {
-            ProfileReader profileReader = new ProfileReader();
-            profiles = profileReader.loadProfiles(pathUtils.getProfilesINIPath());
-        } catch (LoadProfilesException ex) {
+            Collection<Profile> profiles = this.loadProfiles(pathUtils);
+            ProfileLauncher profileLauncher = this.initProfileLauncher(pathUtils);
+            this.initMainWindow(primaryStage, profiles, profileLauncher);
+        } catch (LoadProfilesException | IOException | FirefoxNotInstalledException ex) {
             ex.printStackTrace();
-            System.exit(1);
-        }
-
-        ProfileLauncher profileLauncher = null;
-
-        try {
-            profileLauncher = new ProfileLauncher(
-                    pathUtils.getFirefoxInstallPath(),
-                    pathUtils.getProfilesPath()
-            );
-        } catch (FirefoxNotInstalledException ex) {
-            ex.printStackTrace();
-            System.exit(1);
-        }
-
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainWindow.fxml"));
-            primaryStage.setScene(new Scene(loader.load()));
-            mainWindow = loader.getController();
-            mainWindow.setProfiles(profiles);
-            mainWindow.setMainWindowObserver(new MainWindowController(profileLauncher));
-        } catch (IOException exception) {
-            exception.printStackTrace();
             System.exit(1);
         }
 
         primaryStage.show();
+    }
+
+    /**
+     * Loads profiles from Firefox profiles.ini file.
+     *
+     * @param pathUtils used to get the path to the profiles.ini file
+     *
+     * @return The loaded profiles
+     */
+    private Collection<Profile> loadProfiles(PathUtils pathUtils) throws LoadProfilesException {
+        return new ProfileReader().loadProfiles(pathUtils.getProfilesINIPath());
+    }
+
+    /**
+     * Creates, initializes and returns a profile launcher.
+     * <p>
+     * Specifically, it initializes the Firefox install path and the profiles path.
+     *
+     * @param pathUtils used to get the Firefox install path and the profiles path
+     *
+     * @return An initialized profile launcher
+     *
+     * @throws FirefoxNotInstalledException if the install path of Firefox cannot be detected
+     */
+    private ProfileLauncher initProfileLauncher(PathUtils pathUtils) throws FirefoxNotInstalledException {
+        return new ProfileLauncher(pathUtils.getFirefoxInstallPath(), pathUtils.getProfilesPath());
+    }
+
+    /**
+     * Initializes the main window by loading its FXML file, setting the profiles and setting up its
+     * observers.
+     *
+     * @param primaryStage Stage to set scene to the loaded FXML
+     * @param profiles     Collection of profiles to give main window access to
+     * @param launcher     Profile launcher
+     *
+     * @throws IOException If the FXML file cannot be loaded
+     */
+    private void initMainWindow(
+            Stage primaryStage, Collection<Profile> profiles, ProfileLauncher launcher
+    ) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainWindow.fxml"));
+        primaryStage.setScene(new Scene(loader.load()));
+        MainWindow mainWindow = loader.getController();
+        mainWindow.setProfiles(profiles);
+        mainWindow.setMainWindowObserver(new MainWindowController(launcher));
     }
 }
