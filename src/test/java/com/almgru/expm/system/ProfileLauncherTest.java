1246aab0ccd3e1f07bff85fb304fae2d1d9cbfd3
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-20 01:04:16 +0100

    Add launchProfile test case description

---
+++ src/test/java/com/almgru/expm/system/ProfileLauncherTest.java
@@ -0,0 +1,10 @@
+package com.almgru.expm.system;
+
+public class ProfileLauncherTest {
+    /*
+     * Test case
+     *
+     * launchProfile should pass path of profile to executer when profile is
+     * absolute.
+     */
+}
