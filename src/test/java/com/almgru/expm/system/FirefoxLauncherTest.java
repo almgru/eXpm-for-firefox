aa796e94347d62b863243721edce22cf7c7ee02c
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-02-05 17:20:41 +0100

    Adds test cases for FirefoxLauncher

---
+++ src/test/java/com/almgru/expm/system/FirefoxLauncherTest.java
@@ -0,0 +1,24 @@
+package com.almgru.expm.system;
+
+public class FirefoxLauncherTest {
+    /*
+     * Test case
+     *
+     * constructor should throw exception if any argument is null.
+     */
+
+    /*
+     * Test case
+     *
+     * launchFirefox should throw exception if FireFox not installed.
+     *
+     * Variants:
+     *  - throw exception if path to firefox is not valid.
+     */
+
+    /*
+     * Test case
+     *
+     * launchFirefox should throw exception if arguments are invalid.
+     */
+}
