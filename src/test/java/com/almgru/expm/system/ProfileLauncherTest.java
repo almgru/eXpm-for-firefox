014cd79881cf039fbbe91ac374e704df5dfd25b0
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-02-06 16:09:35 +0100

    Defines test cases for ProfileLauncher

--- src/test/java/com/almgru/expm/system/ProfileLauncherTest.java
+++ src/test/java/com/almgru/expm/system/ProfileLauncherTest.java
@@ -4,6 +4,12 @@ public class ProfileLauncherTest {
     /*
      * Test case
      *
+     * constructor should throw exception if any argument is null.
+     */
+
+    /*
+     * Test case
+     *
      * launchProfile should pass path of profile to executor when profile is
      * absolute.
      */
@@ -13,5 +19,25 @@ public class ProfileLauncherTest {
      *
      * launchProfile should pass path of profile to executor when profile is
      * relative.
+     */
+
+    /*
+     * Test case
+     *
+     * launchProfile should throw exception if profile is null.
+     */
+
+    /*
+     * Test case
+     *
+     * launchProfile should throw failed to launch profile exception if
+     * FirefoxLauncher.launchFirefox throws exception.
+     */
+
+    /*
+     * Test case
+     *
+     * launchProfile should throw failed to launch profile exception if
+     * path to profile is invalid.
      */
 }
